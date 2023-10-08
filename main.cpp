e1fd7d30c6cbbcef6170cf779ef43809afaf4305
Daniel Santos <danieldsm.on@gmail.com>
2023-10-08 20:07:07 -0300

    Criando principais estruturas necessárias para implementação e métodos que serão implementados

--- main.cpp
+++ main.cpp
@@ -1,3 +1,30 @@
+template <typename T>
+class ABB
+{
+private:
+    struct Node
+    {
+        T chave;
+        Node *pai, *esq, *dir;
+    };
+    Node *raiz;
+public:
+    ABB() : raiz{nullptr} {}
+    ~ABB() {}
+
+    void emOrdem();
+
+    Node* buscar();
+    Node* min();
+    Node* max();
+    Node* sucessor();
+    Node* predecessor();
+    
+    void incluir();
+    void remover();
+};
+
+
 int main () 
 {
     return 0;
